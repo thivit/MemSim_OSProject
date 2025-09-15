8bb43f02b6bdd5dd85a8e4cf97e48b657a6a4b22
thivit <114897003+thivit@users.noreply.github.com>
2025-09-15 12:08:21 +0930

    Header file definitions

---
+++ memsim.h
@@ -0,0 +1,35 @@
+#ifndef MEMSIM_H
+#define MEMSIM_H
+
+// page structure
+typedef struct 
+{
+    int pageNo;
+    int modified;
+} page;
+
+// modes of page replacement
+enum repl 
+{
+    random,
+    fifo,
+    lru,
+    clock
+};
+
+// shared variables declared in mem.c
+extern int numFrames;
+extern page* frameTable;
+
+// main functions
+int createMMU(int frames);
+int checkInMemory(int page_number);
+int allocateFrame(int page_number);
+
+// policy selectors
+page selectVictimFIFO(int page_number);
+page selectVictimLRU(int page_number);
+page selectVictimRand(int page_number);
+page selectVictimClock(int page_number);
+
+#endif
